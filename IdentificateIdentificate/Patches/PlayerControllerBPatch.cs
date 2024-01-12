3033c4ce8b58b8872b4220fe446e9a4bb59663ae
Caby <cabydelgado@hotmail.es>
2024-01-12 22:50:21 +0100

    Mod Start
    Player dies when doing Emote 2

---
+++ IdentificateIdentificate/Patches/PlayerControllerBPatch.cs
@@ -0,0 +1,24 @@
+﻿using BepInEx.Logging;
+using GameNetcodeStuff;
+using HarmonyLib;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IdentificateIdentificate.Patches
+{
+    [HarmonyPatch(typeof(PlayerControllerB))]
+
+    internal class PlayerControllerBPatch
+    {
+
+        [HarmonyPatch("Emote2_performed")]
+        [HarmonyPostfix]
+        static void Emote2_performed_Patch(ref bool ___isPlayerDead)
+        {
+            ___isPlayerDead = true;
+        }
+    }
+}
--- IdentificateIdentificate/Plugin.cs
+++ IdentificateIdentificate/Plugin.cs
@@ -1,9 +1,37 @@
-﻿using System;
+﻿using BepInEx;
+using BepInEx.Logging;
+using HarmonyLib;
+using IdentificateIdentificate.Patches;
+using System;
 
 namespace IdentificateIdentificate
 {
-    public class IdentificateIdentificateBase
+    [BepInPlugin(modGUID, modName, modVersion)]
+    public class IdentificateIdentificateBase : BaseUnityPlugin
     {
+        private const string modGUID = "CabyDJ.IdentificateIdentificate";
+        private const string modName = "Identificate Identificate";
+        private const string modVersion = "1.0.0.0";
 
+        private readonly Harmony harmony = new Harmony(modGUID);
+
+        private static IdentificateIdentificateBase instance;
+
+        internal ManualLogSource mls;
+
+        void Awake()
+        {
+            if (instance == null)
+            {
+                instance = this;
+            }
+
+            mls = BepInEx.Logging.Logger.CreateLogSource(modGUID);
+
+            mls.LogInfo("(º)< Identificate! Awake");
+
+            harmony.PatchAll(typeof(IdentificateIdentificateBase));
+            harmony.PatchAll(typeof(PlayerControllerBPatch));
+        }
     }
 }
